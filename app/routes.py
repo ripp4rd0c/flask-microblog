62fb5adb65168229661af8312b066c429727ffdd
ripp4rd0c <hououinkyoma13@protonmail.com>
2022-11-27 14:52:17 +0200

    added receiving form data functionality

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -18,7 +18,13 @@ def index():
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    form = LoginForm()
+    form = LoginForm() # create form object
+    
+    # check if form is submitted, if it is, validate the form and redirect to index page 
+    if form.validate_on_submit():
+        flash(f"login requested for {form.username.data}\nremember me = {form.remember_me.data}")
+        return redirect('/index')
+
     return render_template('login.html', form=form, title='Sign In')
